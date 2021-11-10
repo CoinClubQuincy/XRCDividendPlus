0ec1c0babc95841cd1c54ee5af4dd9cbd0d0269c
MAC-Repo <rqjones@asu.edu>
2021-11-09 18:26:50 -0700

    XRC

--- Contracts/XRC-1170.sol
+++ Contracts/XRC-1170.sol
@@ -4,7 +4,7 @@
 //import "@openzeppelin/contracts/token/XRC1155/XRC1155.sol";
 
 
-contract XRC1170 is XRC1155 {
+contract XRC1170 is ERC1155 {
     uint256 public constant SHARDS = 0;
     string public Name;
     uint counter =0;
@@ -16,7 +16,7 @@ contract XRC1170 is XRC1155 {
         address payable Address;
     }
     
-    constructor(string memory _name,uint _Shards, string memory _uri) public XRC1155(_uri) {
+    constructor(string memory _name,uint _Shards, string memory _uri) public ERC1155(_uri) {
         Name = _name;
         total_supply = _Shards;
         
