b29e15599b0e8335140d07ac05e9eda9b607a7f5
Q <rqjones@asu.edu>
2021-12-17 13:17:17 -0700

    New Accounting CoincBank => Treasury => Account

--- Contracts/XRC20Plus.sol
+++ Contracts/XRC20Plus.sol
@@ -3,10 +3,19 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+abstract contract CoinBank{
+    uint coinbanktotal = address(this).balance;
+    uint Shard_yeild_depisit;
+    uint supply;
+    //function Add_To_Treasury(){}
+}
 
-contract Plus is ERC20 {
+
+
+contract Plus is ERC20, CoinBank {
     uint counter =0;
     uint Account_Counter = 0;
+
     //mappings map Account amounts and micro ledger
     mapping (address => Accounts) public accounts;
     mapping (uint => micro_ledger) public ledger;
@@ -20,16 +29,25 @@ contract Plus is ERC20 {
         address account;
     }
     //launch Contract
-    constructor(string memory name,string memory symbol,uint256 totalSupply) ERC20(name, symbol) {
+    constructor(string memory name,string memory symbol,uint totalSupply) ERC20(name, symbol) {
         _mint(msg.sender, uint(totalSupply));
+        supply = uint(totalSupply);
+
     }
     //Test logging and accounting user dividends
-    function Submit(address _reciver,uint _amount) public{}   
+    function Add_to_Micro_ledger() internal{
+        if(accounts[msg.sender].exist == true){
+            //do nothing
+        } else {
+            ledger[Account_Counter] = micro_ledger(msg.sender);
+            accounts[msg.sender] = Accounts(0,true);
+            Account_Counter++;
+        }
+    }   
     //Account of your funds in contract
-    function CoinBank() public view returns(string memory){
+    function View_CoinBank_Ammount() public view returns(uint){
         return accounts[msg.sender].ammount;
     }
-
     //call contract balance
     function Balance() public returns (uint256) {
         return address(this).balance;
