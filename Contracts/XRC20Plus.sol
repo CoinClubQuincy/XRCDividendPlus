d839d05d98851eb5865f1b876da99cb4dd37b903
Q <rqjones@asu.edu>
2022-01-25 20:25:47 -0700

    added contingency to dividends

--- Contracts/XRC20Plus.sol
+++ Contracts/XRC20Plus.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 //-------------------------- CoinBank Accounting Contract --------------------------
 contract CoinBank{
+    address public CoinBankAddress=address(this); // When coinbank is launched add address <--Here
     uint coinbanktotal = address(this).balance;
     uint Shard_yeild_deposit; //votable
     uint supply;
@@ -43,6 +44,7 @@ contract CoinBank{
             uint single_Shard = uint(Total_from_Bank/supply);
             Issue_To_Treasury(single_Shard);
             //Call Accept from CoinBank
+
         }
     }
 }
@@ -54,7 +56,7 @@ abstract contract Plus is ERC20, CoinBank,Accept_From_CoinBank_Interface {
     uint counter =0;
     uint Account_Counter = 0;
 
-    address public CoinBankAddress=address(this); // When coinbank is launched add address <--Here
+    
     //mappings map Account amounts and micro ledger
     mapping (address => Accounts) public accounts;
     mapping (uint => micro_ledger) public ledger;
@@ -107,14 +109,23 @@ abstract contract Plus is ERC20, CoinBank,Accept_From_CoinBank_Interface {
         return address(this).balance;
     }
     //Accept payment from CoinBank and issue dividends to accouts
-    function Accept_From_CoinBank(uint _singleShard)external payable CoinBankOnly{
+    function Accept_From_CoinBank(uint _singleShard)public payable CoinBankOnly{
+        uint value = msg.value;
+        uint ShardCounter=0;
         uint i=0;
         for(i;i>=Account_Counter;i++){
             address Serach_result = ledger[Account_Counter].account;
             if(ledger[Account_Counter].exist == true && accounts[Serach_result].ammount > 0){
                 accounts[Serach_result].ammount += balanceOf(ledger[Account_Counter].account) * _singleShard;
+                ShardCounter += balanceOf(ledger[Account_Counter].account) * _singleShard;
             }
         }
+        //refactor leftovers from unregisterd account & assimilate funds into treasury
+        if(ShardCounter != value){
+            uint leftovers = value - ShardCounter;
+            Accept_From_CoinBank(leftovers);
+        }
+
     }
     //Redeem Dividends from treasury
     function Redeem()public {
