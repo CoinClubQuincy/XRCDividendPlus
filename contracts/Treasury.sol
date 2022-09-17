ff3ae6ca1274d92a528550f476312029535cdc73
Quincy Jones <rqjones@asu.edu>
2022-09-17 12:57:09 -0700

    notes & renaming

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -2,25 +2,19 @@ pragma solidity ^0.8.10;
 // SPDX-License-Identifier: MIT
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-//-------------------------- Treasury Contract --------------------------
-interface Plus_Interface {
+interface Treasury_Interface {
     function View_Account() external view returns(uint);  // -- ✓
     function Redeem()external returns(bool);              // -- ✓
     function Register_Account()external returns(bool);    // -- ✓
     function CountRegisterdShards()external returns(uint);//
 }
-contract MainTreasury is ERC20, Plus_Interface {
+contract Treasury is ERC20, Treasury_Interface {
     uint counter =0;
     uint public Account_Counter = 0;
     uint public totalAllocated;
-    address public CoinBank_Contract;
-    //every period a time event will be placecd
-    event TreasuryClock( uint256,bool);
-
     //mappings map Account amounts and micro ledger
     mapping (address => Accounts) public accounts;
     mapping (uint => Ledger) public ledger;
-    
     //Account Details
     struct Accounts{
         uint amount;
@@ -32,15 +26,10 @@ contract MainTreasury is ERC20, Plus_Interface {
         bool exist;
     }
     //launch Contract
-    constructor(string memory name,string memory symbol,uint totalSupply,uint8 decimals,uint _Interval) ERC20(name, symbol) {        
+    constructor(string memory name,string memory symbol,uint totalSupply,uint8 decimals,uint _Interval) ERC20(name, symbol) {
         totalSupply = totalSupply*(10**decimals);
         _mint(msg.sender, uint(totalSupply));
         Register_Account();
-    }
-    //require coinbank 
-    modifier FallbackOnly{
-        require(msg.sender == address(this), "only contract can call this");
-        _;
     }
     //Test logging and accounting user dividends
     function Register_Account() public returns(bool){
@@ -56,7 +45,7 @@ contract MainTreasury is ERC20, Plus_Interface {
         return accounts[msg.sender].amount;
     }
     //Accept payment from CoinBank and issue dividends to accounts
-    function Accept_From_CoinBank(uint _singleShard)internal{
+    function callFromFallback(uint _singleShard)internal{
         totalAllocated=0;
         uint amountAllocated;
         uint CurrentCount=0;
@@ -65,9 +54,8 @@ contract MainTreasury is ERC20, Plus_Interface {
             (CurrentCount,amountAllocated) = InternalAccounting(CurrentCount,_singleShard);
             totalAllocated += amountAllocated;
         }
-        emit TreasuryClock(block.timestamp,true); 
     }
-    //counts all the accounts in the Treasury
+    //Counts all the accounts in the Treasury
     function CountRegisterdShards() public view returns(uint){
         uint count =0;
         uint totalAmount=0;
@@ -97,13 +85,11 @@ contract MainTreasury is ERC20, Plus_Interface {
         RedeemAddress.transfer(redeemValue);
         return true;     
     }
-    function Issue_ToTreasury(uint _single_Shard)internal{
-        Accept_From_CoinBank(_single_Shard); //place treasury contract address here
-    }
+    //Payments made to the contract
     fallback() external payable {
         uint Supply = CountRegisterdShards();
         // add one timeInterval to the int 60
         uint single_Shard = msg.value/Supply;
-        Issue_ToTreasury(single_Shard); //Call Accept from CoinBank
+        callFromFallback(single_Shard); //place treasury contract address here(single_Shard); //Call Accept from CoinBank
     }
 }
